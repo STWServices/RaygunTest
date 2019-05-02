4e6f08b14b9c5f94a9499eca575c9fd02c1190e8
Swati <swati@softechworld.com>
2019-05-02 21:30:42 +0530

    Raygun Test

---
+++ RaygunTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Mindscape.Raygun4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RaygunTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //generate error
+
+            decimal d = Convert.ToDecimal("throwerror");
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ RaygunTest/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Mindscape.Raygun4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RaygunTest
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exc = Server.GetLastError();
+            new RaygunClient().SendInBackground(exc, new List<string>() { "WebUI" });
+        }
+    }
+}
